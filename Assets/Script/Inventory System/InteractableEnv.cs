9b9578844a68886712ebf42f726270743271c56f
OrangeEgg1937 <1155147738@link.cuhk.edu.hk>
2023-10-25 07:41:09 +0800

    Demo Environment Setup
    Create Different object

---
+++ Assets/Script/Inventory System/InteractableEnv.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The object in the environment that can be interacted with
+public class InteractableEnv : InteractableObject
+{
+    public override void Interact()
+    {
+        
+    }
+
+    public override void Interact(GameObject target)
+    {
+       
+    }
+
+    public override void Upgrade(GameObject source)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
